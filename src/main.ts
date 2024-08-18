e63a07fe53adaaa2659f4fe4c7bf36aba5116759
zana-abdollahpour <zana.ap4@gmail.com>
2024-08-18 19:44:04 +0330

    02 - glowing text

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,14 @@
 import "./style.css";
 
-// const mainContainer = document.getElementById("app");
+const text = document.querySelector(".text") as HTMLHeadingElement;
+const spans = text.innerText
+  .split("")
+  .map(
+    (char, i) =>
+      `<span style="transition-delay: ${i * 50}ms;filter: hue-rotate(${
+        i * 30
+      }deg)">${char}</span>`
+  )
+  .join("");
+
+text.innerHTML = spans;
