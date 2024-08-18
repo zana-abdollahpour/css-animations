77130fca9b5b7a63ca4643cc4ff86123b7cd7615
zana-abdollahpour <zana.ap4@gmail.com>
2024-08-18 20:03:11 +0330

    parallax jungle

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 import "./style.css";
 
-const text = document.querySelector(".text") as HTMLHeadingElement;
-const spans = text.innerText
-  .split("")
-  .map(
-    (char, i) =>
-      `<span style="transition-delay: ${i * 50}ms;filter: hue-rotate(${
-        i * 30
-      }deg)">${char}</span>`
-  )
-  .join("");
+const text = document.getElementById("text") as HTMLHeadingElement;
+const leaf = document.getElementById("leaf") as HTMLImageElement;
+const hill1 = document.getElementById("hill1") as HTMLImageElement;
+const hill4 = document.getElementById("hill4") as HTMLImageElement;
+const hill5 = document.getElementById("hill5") as HTMLImageElement;
 
-text.innerHTML = spans;
+window.addEventListener("scroll", () => {
+  const { scrollY } = window;
+  console.log(scrollY);
+
+  text.style.marginTop = `${scrollY * 2.5}px`;
+  leaf.style.top = `${scrollY * -1.5}px`;
+  leaf.style.left = `${scrollY * 1.5}px`;
+  hill5.style.left = `${scrollY * 1.5}px`;
+  hill4.style.left = `${scrollY * -1.5}px`;
+  hill1.style.top = `${scrollY * 1}px`;
+});
